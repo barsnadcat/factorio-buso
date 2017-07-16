c067efe4d4f730d3b2dfcaeffea794f1b09da198
barsandcat <barsandcat@gmail.com>
2017-07-16 09:11:59 +0300

    Filter recepies a little

--- main.py
+++ main.py
@@ -1,15 +1,133 @@
 import json
 import os
 
+whiteList = set(["advanced-oil-processing",
+"accumulator",
+"advanced-circuit",
+"assembling-machine-1",
+"assembling-machine-2",
+"battery",
+"big-electric-pole",
+"boiler",
+"cargo-wagon",
+"chemical-plant",
+"concrete",
+"construction-robot",
+"copper-cable",
+"copper-plate",
+"electric-engine-unit",
+"electric-mining-drill",
+"electronic-circuit",
+"engine-unit",
+"express-splitter",
+"express-transport-belt",
+"express-underground-belt",
+"fast-inserter",
+"fast-splitter",
+"fast-transport-belt",
+"fast-underground-belt",
+"filter-inserter",
+"firearm-magazine",
+"fluid-wagon",
+"flying-robot-frame",
+"gate",
+"grenade",
+"gun-turret",
+"hazard-concrete",
+"high-tech-science-pack",
+"inserter",
+"iron-chest",
+"iron-gear-wheel",
+"iron-plate",
+"iron-stick",
+"lab",
+"laser-turret",
+"locomotive",
+"logistic-chest-passive-provider",
+"logistic-chest-storage",
+"logistic-robot",
+"long-handed-inserter",
+"low-density-structure",
+"lubricant",
+"medium-electric-pole",
+"military-science-pack",
+"offshore-pump",
+"oil-refinery",
+"piercing-rounds-magazine",
+"pipe",
+"pipe-to-ground",
+"plastic-bar",
+"processing-unit",
+"production-science-pack",
+"pumpjack",
+"rail",
+"rail-chain-signal",
+"rail-signal",
+"repair-pack",
+"roboport",
+"rocket-control-unit",
+"rocket-fuel",
+"rocket-part",
+"science-pack-1",
+"science-pack-2",
+"science-pack-3",
+"small-electric-pole",
+"solar-panel",
+"speed-module",
+"splitter",
+"stack-filter-inserter",
+"stack-inserter",
+"steam-engine",
+"steel-chest",
+"steel-furnace",
+"steel-plate",
+"stone-brick",
+"stone-furnace",
+"stone-wall",
+"storage-tank",
+"substation",
+"sulfur",
+"sulfuric-acid",
+"train-stop",
+"transport-belt",
+"underground-belt",
+"wood",
+"rocket-silo",
+"satellite",
+"electric-furnance"])
+
+resourceList = set(["raw-wood", "water", "solid-fuel", "radar", "iron-ore", "copper-ore", "coal", "crude-oil", "heavy-oil", "stone"])
 
 def main():
-    with open('recipes.json') as fp:
-        recipes = json.load(fp)
-        for name, recipe in recipes.items():
-        	print(recipe["name"])
-        	print(recipe["ingredients"])
+	with open('recipes.json') as fp:
+		recipes = json.load(fp)
+		ingredientUsage = {}
+		
+		for name, recipe in recipes.items():
+			if (name in whiteList):
+				for ingredient in recipe["ingredients"]:
+					ingreidentName = ingredient["name"]
+					if ingreidentName not in resourceList:
+						ingredientUsage[ingreidentName] = ingredientUsage.get(ingreidentName, 0) + 1
+			else:
+				print(name)
 
-        
+
+		for ingredient in sorted(ingredientUsage, key=ingredientUsage.get):
+			print(ingredient)
+			print(ingredientUsage[ingredient])
+
+
+		
+## udpate bus
+## find out aviable recipies with current bus
+## add leaf recepies
+## add aviable line closing recepies
+## add new line recepie
+##     with most leaf consumers?
+##     with most line closing consumers?
+##     with most consumers?
+## ...
 
 if __name__ == '__main__':
-    main()
+	main()
