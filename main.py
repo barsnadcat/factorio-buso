9a90cac906141f057d4ba756ac46e578e28c8367
Dmitriy Tsahilov <Dmitriy.Ts@fraglab.local>
2017-10-20 10:01:15 +0300

    Replace recipe list with blocked recipes insteadq

--- main.py
+++ main.py
@@ -1,97 +1,9 @@
 import json
 import os
 
-allowedRecipes = set(["basic-oil-processing",
-"accumulator",
-"advanced-circuit",
-"assembling-machine-1",
-"assembling-machine-2",
-"battery",
-"big-electric-pole",
-"medium-electric-pole",
-"small-electric-pole",
-"boiler",
-"chemical-plant",
-"concrete",
-"construction-robot",
-"copper-cable",
-"copper-plate",
-"electric-engine-unit",
-"electric-mining-drill",
-"electronic-circuit",
-"engine-unit",
-"fast-inserter",
-"fast-splitter",
-"fast-transport-belt",
-"fast-underground-belt",
-"firearm-magazine",
-"fluid-wagon",
-"cargo-wagon",
-"train-stop",
-"pump",
-"locomotive",
-"gate",
-"grenade",
-"gun-turret",
-"high-tech-science-pack",
-"inserter",
-"iron-chest",
-"iron-gear-wheel",
-"iron-plate",
-"iron-stick",
-"lab",
-"laser-turret",
-"flying-robot-frame",
-"logistic-chest-passive-provider",
-"logistic-chest-storage",
-"logistic-robot",
-"long-handed-inserter",
-"low-density-structure",
-"lubricant",
-"military-science-pack",
-"offshore-pump",
-"piercing-rounds-magazine",
-"pipe",
-"pipe-to-ground",
-"plastic-bar",
-"processing-unit",
-"production-science-pack",
-"pumpjack",
-"rail",
-"repair-pack",
-"roboport",
-"rocket-control-unit",
-"rocket-fuel",
-"rocket-part",
-"science-pack-1",
-"science-pack-2",
-"science-pack-3",
-"solar-panel",
-"speed-module",
-"splitter",
-"steam-engine",
-"steel-chest",
-"steel-furnace",
-"steel-plate",
-"stone-brick",
-"stone-furnace",
-"stone-wall",
-"storage-tank",
-"sulfur",
-"sulfuric-acid",
-"transport-belt",
-"underground-belt",
-"wood",
-"flamethrower-ammo",
-"landfill",
-"rocket-silo",
-"satellite",
-"electric-furnace",
-"solid-fuel-from-heavy-oil",
-"radar",
-"rail-signal",
-"rail-chain-signal"])
-
+blockedRecipes = set(["advanced-oil-processing", "heavy-oil-cracking", "light-oil-cracking", "coal-liquefaction",
+"empty-crude-oil-barrel", "empty-heavy-oil-barrel", "empty-light-oil-barrel", "empty-lubricant-barrel", "empty-petroleum-gas-barrel", "empty-sulfuric-acid-barrel", "empty-water-barrel", 
+"solid-fuel-from-light-oil", "solid-fuel-from-petroleum-gas" ])
 
 resourceList = set(["raw-wood", "water", "iron-ore", "copper-ore", "coal", "crude-oil", "stone"])
 
@@ -117,7 +29,7 @@ def main():
     with open('recipes.json') as recipefp:
         global recipesJson
         recipesJson = json.load(recipefp)
-        
+                
         bus = {}
 
         for product in targetProductList:
@@ -148,24 +60,23 @@ def RemoveFromBus(product, bus):
         print("Del", product, bus[product])
         del bus[product]
     else:
-        recipe = FindRecipeByProduct(product)
-        recipeJson = recipesJson[recipe]
+        recipeJson = FindRecipeByProduct(product)
         usage = bus[product]
 
         ## Adding/updating usage of ingredients to bus
         AddToBus(recipeJson, bus, usage)
 
         ## Removing product from bus
-        print("Del", product, bus[product], recipe)
+        print("Del", product, bus[product], recipeJson["name"])
         del bus[product]
 
 
-def FindRecipeByProduct(product):
-    for recipe in allowedRecipes:
-        recipeJson = recipesJson[recipe]
-        for productJson in recipeJson["products"]:
-            if product == productJson["name"]:
-                return recipe
+def FindRecipeByProduct(product):    
+    for recipeName, recipeJson in recipesJson.items():
+        if recipeName not in blockedRecipes:
+            for productJson in recipeJson["products"]:
+                if product == productJson["name"]:
+                    return recipeJson
         
 
 
